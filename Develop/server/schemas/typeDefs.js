a98b3c9f8a7412f7ac3b72a717cc1fcb7eb4cf1e
David Rodriguez <Daviderrr10@gmail.com>
2023-07-13 15:56:03 -0400

    types added

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,1 +1,25 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Float
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID
+    title: String
+    author: String
+    publicationDate: Int
+    savedBooks: Professor
+  }
+
+  type Auth {
+    Token: []
+    user: 
+    
+  }
+`;
+
