1819ce54394aca77b0605b4250f6faa553bf34fc
David Rodriguez <Daviderrr10@gmail.com>
2023-07-13 16:20:36 -0400

    user query

--- Develop/client/src/utils/queries.js
+++ Develop/client/src/utils/queries.js
@@ -2,3 +2,19 @@ import { gql } from '@apollo/client';
 
 
 export const GET_USER = gql`
+  query User {
+    user {
+      _id
+      email
+      username
+      bookCount
+      saveBooks {
+        authors
+        bookId
+        description
+        title
+        image
+        link
+      }
+    }
+  };`
