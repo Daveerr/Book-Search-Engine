3e9ef148cbafa14c11f10820b02889d47205639f
David Rodriguez <Daviderrr10@gmail.com>
2023-07-25 09:37:51 -0400

    fixing local host error

--- Develop/client/src/index.js
+++ Develop/client/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/", //  GraphQL server endpoint
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
